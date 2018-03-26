c152e1130e49b8be9c2afb75c7cc3b74028b88dc
Klaus-Peter_Schreiner <srp@Viessmann.com>
2018-03-26 17:04:24 +0200

    Change build_vim8.bat to use env variables

--- bin/build_vim8.bat
+++ bin/build_vim8.bat
@@ -1,8 +1,14 @@
 @echo off
 
 REM configuration
-set VIM_SRC=c:\work\vim8\vim
-set VIM_BIN=c:\opt\vim8
+if not "%1"=="" set VIM_SRC=%1
+if not "%2"=="" set VIM_BIN=%2
+if "%VIM_SRC%"=="" set VIM_SRC=c:\work\vim8\vim
+if "%VIM_BIN%"=="" set VIM_BIN=c:\opt\vim8
+echo.
+echo    Source directory: %VIM_SRC%
+echo    Target directory: %VIM_BIN%
+echo.
 
 pushd %VIM_SRC%\src
 
